cab2fb3f79617544e5a30497746aff5b7fbf964d
saku shirakura <saku@sakushira.com>
2025-05-14 16:27:38 +0900

    [Fix] Fix SQL Error DBManager::deactivateAllTasks

--- src/core/DBManager.cpp
+++ src/core/DBManager.cpp
@@ -705,7 +705,7 @@ namespace core::db {
     {
         WorktimeTable table;
         return table.usePlaceholderUniSql(
-            "UPDATE worktime SET finishing_time = (strftime('%s', DATETIME('now'))) WHERE finishing_time IS NULL ORDER BY id DESC;");
+            "UPDATE worktime SET finishing_time = (strftime('%s', DATETIME('now'))) WHERE finishing_time IS NULL;");
     }
 
     int WorktimeTable::selectActiveTask()
