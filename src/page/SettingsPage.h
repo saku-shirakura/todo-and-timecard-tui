700915dfb1e48aecfd08b7e5b4c2e9207ef81553
saku shirakura <saku@sakushira.com>
2025-06-14 23:10:56 +0900

    [Add/WIP] Add the SettingsPage. This page is not implemented.

---
+++ src/page/SettingsPage.h
@@ -0,0 +1,49 @@
+// MIT License
+//
+// Copyright (c) 2025 Saku Shirakura <saku@sakushira.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+/**
+ * @file SettingsPage.h
+ * @date 25/06/14
+ * @brief ファイルの説明
+ * @details ファイルの詳細
+ * @author saku shirakura (saku@sakushira.com)
+ */
+
+
+#ifndef SETTINGSPAGE_H
+#define SETTINGSPAGE_H
+#include "../../cmake-build-release/_deps/ftxui-src/include/ftxui/component/component.hpp"
+#include "../components/TodoListPageComponents/TodoListPageComponents.h"
+
+namespace pages {
+    /**
+     * @brief ここにクラスの説明
+     * @details ここにクラスの詳細な説明
+     * @since
+     */
+    class SettingsPage final {
+    public:
+        ftxui::Component getComponent();
+    };
+} // pages
+
+#endif //SETTINGSPAGE_H
