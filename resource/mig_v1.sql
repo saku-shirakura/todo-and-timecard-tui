eef1c4a80cd01e32d59004e552998fe3cb2ebd3a
saku shirakura <saku@sakushira.com>
2025-06-17 15:22:09 +0900

    [Update] Update mig_v1.sql. Change time function strftime to unixepoch. Add View null_setted_worktime.

--- resource/mig_v1.sql
+++ resource/mig_v1.sql
@@ -1,19 +1,20 @@
 DROP TABLE IF EXISTS migrate;
 DROP TABLE IF EXISTS settings;
+DROP VIEW IF EXISTS null_setted_worktime;
 
 CREATE TABLE migrate
 (
     id         INTEGER PRIMARY KEY AUTOINCREMENT,
     applied    INTEGER NOT NULL,
-    created_at INTEGER NOT NULL DEFAULT (strftime('%s', DATETIME('now'))),
-    updated_at INTEGER NOT NULL DEFAULT (strftime('%s', DATETIME('now')))
+    created_at INTEGER NOT NULL DEFAULT (unixepoch(DATETIME('now'))),
+    updated_at INTEGER NOT NULL DEFAULT (unixepoch(DATETIME('now')))
 );
 
 CREATE TRIGGER trigger_migrate_updated_at
     AFTER UPDATE
     ON migrate
 BEGIN
-    UPDATE migrate SET updated_at = (strftime('%s', DATETIME('now'))) WHERE rowid == NEW.rowid;
+    UPDATE migrate SET updated_at = (unixepoch(DATETIME('now'))) WHERE rowid == NEW.rowid;
 END;
 
 CREATE TABLE settings
@@ -21,16 +22,25 @@ CREATE TABLE settings
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
     setting_key TEXT    NOT NULL UNIQUE,
     value       TEXT,
-    created_at  INTEGER NOT NULL DEFAULT (strftime('%s', DATETIME('now'))),
-    updated_at  INTEGER NOT NULL DEFAULT (strftime('%s', DATETIME('now')))
+    created_at INTEGER NOT NULL DEFAULT (unixepoch(DATETIME('now'))),
+    updated_at INTEGER NOT NULL DEFAULT (unixepoch(DATETIME('now')))
 );
 
 CREATE TRIGGER trigger_settings_updated_at
     AFTER UPDATE
     ON settings
 BEGIN
-    UPDATE settings SET updated_at = (strftime('%s', DATETIME('now'))) WHERE rowid == NEW.rowid;
+    UPDATE settings SET updated_at = (unixepoch(DATETIME('now'))) WHERE rowid == NEW.rowid;
 END;
+
+CREATE VIEW null_setted_worktime AS
+SELECT id,
+       task_id,
+       starting_time,
+       coalesce(finishing_time, unixepoch(DATETIME('now'))) AS finishing_time,
+       created_at,
+       updated_at
+FROM worktime;
 
 INSERT INTO settings(setting_key, value)
 VALUES ('timezone', '+0000'),
